c123a61bd6ae071b68a43f7f3c6dcf94612dfa9c
EliasR19 <ereynoso2040@gmail.com>
2026-09-09 12:54:19 -0300

    fix: se saca la cantidad de la response

--- api/src/main/java/ar/edu/unq/unqash/venta/VentaResponse.java
+++ api/src/main/java/ar/edu/unq/unqash/venta/VentaResponse.java
@@ -1,21 +1,18 @@
 package ar.edu.unq.unqash.venta;
 
-import ar.edu.unq.unqash.persistencia.TipoCobro;
 import ar.edu.unq.unqash.persistencia.VentaEntity;
 
 import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
+
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.UUID;
+
 
 public record VentaResponse(
                                String producto,
                                BigDecimal monto,
-                               Integer cantidad,
                                String tipoCobro,
                                String fecha,
                                String hora
@@ -35,7 +32,6 @@ public record VentaResponse(
         return new VentaResponse(
                 v.getProducto(),
                 v.getMonto(),
-                v.getCantidad(),
                 v.tipoDeCobro().descripcion(),
                 fecha,
                 hora
