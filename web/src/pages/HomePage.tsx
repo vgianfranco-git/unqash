302789a42c3f1a987489e06dc565daed814ea5b9
agusgarrido <garriagus@gmail.com>
2026-09-09 20:50:37 -0300

    Feature: Se conecta el refresco del listado al registrar una venta.

--- web/src/pages/HomePage.tsx
+++ web/src/pages/HomePage.tsx
@@ -1,11 +1,14 @@
+import { useState } from 'react'
 import VentaForm from '../components/features/VentaForm'
 import ListadoCargas from '../components/features/ListadoCargas'
 
 function HomePage() {
+  const [refreshKey, setRefreshKey] = useState(0)
+
   return (
     <div className="flex flex-col gap-6">
-      <VentaForm />
-      <ListadoCargas />
+      <VentaForm onVentaRegistrada={() => setRefreshKey((key) => key + 1)} />
+      <ListadoCargas refreshKey={refreshKey} />
     </div>
   )
 }
