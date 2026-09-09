33a4644a0c979d7635b5f5f5e9f296ea64fdd4c5
agusgarrido <garriagus@gmail.com>
2026-09-09 19:45:33 -0300

    Feature: Se agrega el listado de operaciones paginado.

--- web/src/pages/HomePage.tsx
+++ web/src/pages/HomePage.tsx
@@ -1,7 +1,13 @@
 import VentaForm from '../components/features/VentaForm'
+import ListadoCargas from '../components/features/ListadoCargas'
 
 function HomePage() {
-  return <VentaForm />
+  return (
+    <div className="flex flex-col gap-6">
+      <VentaForm />
+      <ListadoCargas />
+    </div>
+  )
 }
 
 export default HomePage
