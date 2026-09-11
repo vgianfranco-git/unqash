1cf48e3f748957f3608df9616380caed112823f0
EliasR19 <ereynoso2040@gmail.com>
2026-09-10 21:41:33 -0300

    anular venta endpoint

--- api/src/main/java/ar/edu/unq/unqash/venta/VentaController.java
+++ api/src/main/java/ar/edu/unq/unqash/venta/VentaController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
@@ -46,5 +47,10 @@ public class VentaController {
                 .collect(Collectors.toList()), currVentasPage.getPageable().getPageNumber()+1,
                 currVentasPage.getTotalPages());
 
+    }
+
+    @PostMapping("/{id}/anular")
+    public ResponseEntity<String> anularVenta(@PathVariable UUID id){ //Cambiar a venta
+        return ResponseEntity.status(HttpStatus.CREATED).body("anulado");
     }
 }
